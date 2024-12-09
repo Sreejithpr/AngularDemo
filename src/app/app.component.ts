4d7777e2023e03c1df6c89566feccd25af6b771f
sreejithpr <sreejithpr8@gmail.com>
2024-12-09 11:20:59 +0530

    Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,23 @@
 import { Component } from '@angular/core';
 import { WelcomeComponent } from './welcome/welcome.component';
 import { LoanCalculatorComponent } from './loan-calculator/loan-calculator.component';
+import { TitleFormComponent } from "./title-form/title-form.component";
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
   standalone: true,
-  imports: [WelcomeComponent,LoanCalculatorComponent] // Import the standalone component here
+  imports: [WelcomeComponent, LoanCalculatorComponent, TitleFormComponent] // Import the standalone component here
+ // Import the standalone component here
 })
-export class AppComponent { }
+export class AppComponent { 
+  
+  title:string;
+  constructor(){
+    this.title="Angular SPA 1.O";
+
+  }
+  updateTitle=(title:string)=>{
+    this.title=title;
+  }
+}
--- src/app/welcome/welcome.component.ts
+++ src/app/welcome/welcome.component.ts
@@ -1,32 +1,44 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-
+import { CommonModule } from '@angular/common';
 @Component({
-  selector: 'app-welcome', // Corrected selector
+  selector: 'app-welcome',
   standalone: true,
-  imports: [FormsModule], // Corrected imports array
+  imports: [ FormsModule ,CommonModule],
   templateUrl: './welcome.component.html',
-  styleUrls: ['./welcome.component.css'] // Corrected to styleUrls
+  styleUrl: './welcome.component.css'
 })
 export class WelcomeComponent {
-  userName: string;
-  logos: string[];
-  logoIndex: number;
 
-  constructor() {
-    this.userName = "Somebody";
-    this.logos = [
-      // "assets/images/w1.jpeg",
-      // "assets/images/w2.jpeg",
-      // "assets/images/w3.png"
+  userName:string;
+
+  logos:string[];
+
+  logoIndex:number;
+  logoWidth:number;
+  logoClass:any;
+
+
+  constructor(){
+    this.userName="Somebody";
+    
+    this.logos=[
+      "assets/images/1.png",
+      "assets/images/3.png",
+      "assets/images/123.png",
+      "assets/images/r.png"
     ];
-    this.logoIndex = 0; // Corrected initialization
+
+    this.logoIndex=0;
+    this.logoWidth=300;
+    this.logoClass={border:true,centered:true}
   }
 
-  changeLogo() {
+  changeLogo(){
     this.logoIndex++;
-    if (this.logoIndex === this.logos.length) {
-      this.logoIndex = 0;
+    if(this.logoIndex===this.logos.length){
+      this.logoIndex=0;
     }
   }
+
 }
