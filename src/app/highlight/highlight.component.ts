ccf46c6e45472cc477e8c6783eb9a3c5ec5e5d30
sreejithpr <sreejithpr8@gmail.com>
2024-12-09 11:35:15 +0530

    Anguar commit

---
+++ src/app/highlight/highlight.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-highlight',
+  standalone: true,
+  imports: [],
+  templateUrl: './highlight.component.html',
+  styleUrl: './highlight.component.css'
+})
+export class HighlightComponent {
+
+}
